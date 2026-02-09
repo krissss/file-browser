5ef84d3d2ba2ede3d21eeca44df87546fcd42b37
kriss <462679766@qq.com>
2026-02-09 16:00:14 +0800

    fix: restore cli entrypoint
    Add cmd/file-browser/main.go back so Docker builds can copy the CLI entrypoint.

---
+++ cmd/file-browser/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"reflect"
+	"strings"
+
+	"file-browser/internal/server"
+)
+
+func formatConfig(cfg server.Config) string {
+	value := reflect.ValueOf(cfg)
+	typ := value.Type()
+	parts := make([]string, 0, typ.NumField())
+	for i := 0; i < typ.NumField(); i++ {
+		field := typ.Field(i)
+		name := toKebab(field.Name)
+		parts = append(parts, fmt.Sprintf("%s=%v", name, value.Field(i).Interface()))
+	}
+	return strings.Join(parts, " ")
+}
+
+func toKebab(input string) string {
+	var b strings.Builder
+	for i, r := range input {
+		if i > 0 && r >= 'A' && r <= 'Z' {
+			b.WriteByte('-')
+		}
+		b.WriteRune(r)
+	}
+	return strings.ToLower(b.String())
+}
+
+func main() {
+	cfg, err := server.ParseConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	srv, err := server.New(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("file-browser: %s", formatConfig(cfg))
+	if err := http.ListenAndServe(cfg.Addr(), srv.Handler()); err != nil {
+		log.Fatal(err)
+	}
+}
